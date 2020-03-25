89088cf9734344d24e14f6536e1cec94825f27b1
Silv4n <34308038+Silv4n@users.noreply.github.com>
2020-03-25 16:07:08 +0100

    fix, cse edits

--- azdeploy_newfile.ps1
+++ azdeploy_newfile.ps1
@@ -3,3 +3,10 @@ New-Item -ItemType Directory -Name 'AZDEPLOYFOLDERLETSGO' -Path 'C:\'
 
 # Create file
 New-Item -ItemType File -Name 'OMEGALUL.txt' -Path 'C:\AZDEPLOYFOLDERLETSGO'
+
+# Enable WinRM via HTTPS
+Enable-PSRemoting -Force -SkipNetworkProfileCheck
+New-NetFirewallRule -Name "Allow WinRM HTTPS" -DisplayName "WinRM HTTPS" -Enabled True -Profile Any -Action Allow -Direction Inbound -LocalPort 5986 -Protocol TCP
+$thumbprint = (New-SelfSignedCertificate -DnsName $env:COMPUTERNAME -CertStoreLocation Cert:\LocalMachine\My).Thumbprint
+$command = "winrm create winrm/config/Listener?Address=*+Transport=HTTPS @{Hostname=""$env:computername""; CertificateThumbprint=""$thumbprint""}"
+cmd.exe /C $command
