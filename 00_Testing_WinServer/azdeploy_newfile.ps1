420e746bd71a1d93efa42e1601beb6d96a0e4725
Silv4n <34308038+Silv4n@users.noreply.github.com>
2020-03-27 09:42:54 +0100

    fix, redo folder structure

--- 00_Testing_WinServer/azdeploy_newfile.ps1
+++ 00_Testing_WinServer/azdeploy_newfile.ps1
@@ -10,3 +10,6 @@ New-NetFirewallRule -Name "Allow WinRM HTTPS" -DisplayName "WinRM HTTPS" -Enable
 $thumbprint = (New-SelfSignedCertificate -DnsName $env:COMPUTERNAME -CertStoreLocation Cert:\LocalMachine\My).Thumbprint
 $command = "winrm create winrm/config/Listener?Address=*+Transport=HTTPS @{Hostname=""$env:computername""; CertificateThumbprint=""$thumbprint""}"
 cmd.exe /C $command
+
+# Enable RDP on Port 33899
+New-NetFirewallRule -Name "Allow RDP Custom Port" -DisplayName "RDP 33899" -Enabled True -Profile Any -Action Allow -Direction Inbound -LocalPort 33899 -Protocol TCP
---
+++ 01_Standard_WinServer/Start-WinSRVInitial.ps1
@@ -0,0 +1,19 @@
+# Enable WinRM via HTTPS
+Enable-PSRemoting -Force -SkipNetworkProfileCheck
+New-NetFirewallRule -Name "Allow WinRM HTTPS" -DisplayName "WinRM HTTPS" -Enabled True -Profile Any -Action Allow -Direction Inbound -LocalPort 5986 -Protocol TCP
+$thumbprint = (New-SelfSignedCertificate -DnsName $env:COMPUTERNAME -CertStoreLocation Cert:\LocalMachine\My).Thumbprint
+$command = "winrm create winrm/config/Listener?Address=*+Transport=HTTPS @{Hostname=""$env:computername""; CertificateThumbprint=""$thumbprint""}"
+cmd.exe /C $command
+
+# Set timezone
+Set-TimeZone -Id "W. Europe Standard Time"
+
+# Initialize data disk
+Initialize-Disk -Number 2
+New-Partition -DiskNumber 2 –UseMaximumSize -GptType -DriveLetter F
+Format-Volume –DriveLetter F -FileSystem NTFS -NewFileSystemLabel "Data"
+
+# Disable IE Enhanced security for Admins
+$AdminKey = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}”
+Set-ItemProperty -Path $AdminKey -Name “IsInstalled” -Value 0
+Stop-Process -Name Explorer
