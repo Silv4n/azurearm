dcb7af0ca2c1c4caab131b6e61d836c437bb4660
Silv4n <34308038+Silv4n@users.noreply.github.com>
2020-03-27 11:32:39 +0100

    fix, ps script

--- 01_Standard_WinServer/Start-WinSRVInitial.ps1
+++ 01_Standard_WinServer/Start-WinSRVInitial.ps1
@@ -10,10 +10,10 @@ Set-TimeZone -Id "W. Europe Standard Time"
 
 # Initialize data disk
 Initialize-Disk -Number 2
-New-Partition -DiskNumber 2 –UseMaximumSize -GptType -DriveLetter F
+New-Partition -DiskNumber 2 –UseMaximumSize -DriveLetter F
 Format-Volume –DriveLetter F -FileSystem NTFS -NewFileSystemLabel "Data"
 
 # Disable IE Enhanced security for Admins
-$AdminKey = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}”
+$AdminKey = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}”
 Set-ItemProperty -Path $AdminKey -Name “IsInstalled” -Value 0
 Stop-Process -Name Explorer
